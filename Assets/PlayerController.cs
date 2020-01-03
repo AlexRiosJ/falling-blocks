f4b7506fd9769461e3b321decd389ef90ed03a63
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-01-03 02:28:32 -0600

    Creating player controller script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 7;
+
+    float halfPlayerWidth;
+    float screenHalfWidthInWorldUnits;
+
+    // Start is called before the first frame update
+    void Start () {
+        halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if(transform.position.x + halfPlayerWidth < -screenHalfWidthInWorldUnits) {
+            transform.position = new Vector2 (screenHalfWidthInWorldUnits + halfPlayerWidth, transform.position.y);
+        }
+
+        if(transform.position.x - halfPlayerWidth > screenHalfWidthInWorldUnits) {
+            transform.position = new Vector2 (-screenHalfWidthInWorldUnits - halfPlayerWidth, transform.position.y);
+        }
+    }
+}
